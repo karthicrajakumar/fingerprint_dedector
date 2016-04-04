b9305d9844e200165616054790d520ff487d03ce
karthicrajakumar <redrageclan@gmail.com>
2016-04-04 19:30:45 +0530

    date change

--- routes/auth.js
+++ routes/auth.js
@@ -3,14 +3,16 @@ var router = express.Router();
 var User = require('../app/models/user');
 var Requests = require('../app/models/requests');
 var mongoose = require('mongoose');
-
+process.env.TZ = 'Asia/Kolkata';
 router.post('/',function(req,res){
+
   var user_id = req.decoded;
   var user_hash = req.body.hash;
   var post_id = req.body.req_id;
   var mac;
   var add;
   var date = new Date();
+  console.log(date);
   var current_min = date.getMinutes();
 
   User.find({_id:mongoose.Types.ObjectId(user_id)},function(err,docs){
@@ -58,7 +60,10 @@ router.post('/',function(req,res){
 
 
 });
-
+router.get('/',function(req,res){
+  var date = new Date();
+  console.log(date);
+});
 
 
 module.exports = router;
